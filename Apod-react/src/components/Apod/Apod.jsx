7ceb68e064c471586e183a2cc2523b4cd87cea5e
GuilhermeCordeiroDaMata <gcordeiro773@gmail.com>
2022-08-29 20:30:01 -0300

    Pair Programming feat Sara Lirio, implementado a requisição

--- Apod-react/src/components/Apod/Apod.jsx
+++ Apod-react/src/components/Apod/Apod.jsx
@@ -1,36 +1,35 @@
 import React from 'react'
 import { useState } from 'react'
-
+import S from './Apod.module.css'
 const Apod = () => {
-    const [respostaReq, setRespostaReq] = useState({})
+    const [respostaReq, setRespostaReq] = useState()
+    const [data, setData] = useState("")
     async function handleRequisicao(e){
       e.preventDefault()
-        const data = e.target.value
         const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=V4n5887oanZY2NL5JyoEmkhBHHJYmbAhzIJ0TEgJ&date=${data}`)
         const json = await response.json()
         console.log(json);
         setRespostaReq(json)
-        // console.log(respostaReq);
     }
 
-    function handlePicture () {
-      if (data.media_type == "image") {
-       `<br><img width="620px" height="400px" src="${data.url}">`;
-    } else if (data.media_type == "video") {
-      `<br><iframe width="620px" height="400px" src="${data.url}">`;
+    function handleChange(e){
+      setData(e.target.value)
     }
-    }
+
 
   return (
     <form>
         <label>Escolha a data </label>
-        <input type="date" min="1995-06-20" max=""/>
+        <input type="date" min="1995-06-20" max="" onChange={handleChange}/>
         <button onClick={handleRequisicao} >Pesquisa</button>
 
         <p>{respostaReq ? respostaReq.title : "não achei"}</p>
         <p>{respostaReq ? respostaReq.explanation : "não achei"}</p>
-        <iframe src={respostaReq ? respostaReq.url : "não achei"}/>
-        
+        {respostaReq.media_type === "image" ? 
+        <img src={respostaReq.url}/> 
+        : 
+        <iframe src={respostaReq.url} frameborder="0"></iframe>
+      }       
     </form>
   )
 }
