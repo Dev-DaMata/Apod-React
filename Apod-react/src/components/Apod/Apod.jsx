eb58050b2336ba089fa1c0aaeead2fe27f880e40
Sara <saraeliza.lirio@gmail.com>
2022-08-29 19:59:41 -0300

    pair programming - Renderizou title e explanation

--- Apod-react/src/components/Apod/Apod.jsx
+++ Apod-react/src/components/Apod/Apod.jsx
@@ -3,19 +3,34 @@ import { useState } from 'react'
 
 const Apod = () => {
     const [respostaReq, setRespostaReq] = useState({})
-    async function handleRequisicao(key){
-        const response = await fetch("https://api.nasa.gov/planetary/apod?api_key=V4n5887oanZY2NL5JyoEmkhBHHJYmbAhzIJ0TEgJ&date=")
+    async function handleRequisicao(e){
+      e.preventDefault()
+        const data = e.target.value
+        const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=V4n5887oanZY2NL5JyoEmkhBHHJYmbAhzIJ0TEgJ&date=${data}`)
         const json = await response.json()
-        setRespostaReq(json.results[0])
-        console.log(respostaReq);
+        console.log(json);
+        setRespostaReq(json)
+        // console.log(respostaReq);
+    }
+
+    function handlePicture () {
+      if (data.media_type == "image") {
+       `<br><img width="620px" height="400px" src="${data.url}">`;
+    } else if (data.media_type == "video") {
+      `<br><iframe width="620px" height="400px" src="${data.url}">`;
+    }
     }
 
   return (
     <form>
         <label>Escolha a data </label>
-        <p>{respostaReq ? respostaReq.title : ""}</p>
         <input type="date" min="1995-06-20" max=""/>
-        <button onClick={handleRequisicao} ></button>
+        <button onClick={handleRequisicao} >Pesquisa</button>
+
+        <p>{respostaReq ? respostaReq.title : "não achei"}</p>
+        <p>{respostaReq ? respostaReq.explanation : "não achei"}</p>
+        <iframe src={respostaReq ? respostaReq.url : "não achei"}/>
+        
     </form>
   )
 }
